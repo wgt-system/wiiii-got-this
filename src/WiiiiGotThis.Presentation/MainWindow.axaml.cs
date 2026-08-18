fa1d93a62d9d43e7bdf97f2e85e004731e437c1b
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-18 02:26:54 +0200

    feat: configure Desktop Illumination surface source

--- src/WiiiiGotThis.Presentation/MainWindow.axaml.cs
+++ src/WiiiiGotThis.Presentation/MainWindow.axaml.cs
@@ -5,4 +5,10 @@ namespace WiiiiGotThis.Presentation;
 public sealed partial class MainWindow : Window
 {
     public MainWindow() => InitializeComponent();
+
+    public void ConfigureIlluminationProductSurfaceSource(IIlluminationProductSurfaceSource source)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        ShellView.IlluminationProductSurfaceSource = source;
+    }
 }
